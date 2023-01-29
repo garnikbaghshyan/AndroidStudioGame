c806aea09d00bf1a1b7fb8343137b076f8b5661c
garnikbaghshyan <garnikbaghshyan@gmail.com>
2023-01-29 14:30:40 +0400

    Update FPS and UPS.
    Now application displays FPS and UPS correct valuse.

--- app/src/main/java/com/example/androidstudiogame/GameLoop.java
+++ app/src/main/java/com/example/androidstudiogame/GameLoop.java
@@ -4,9 +4,13 @@ import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 
 public class GameLoop extends Thread{
+    private static final double MAX_UPS = 30.0;
+    private static final double UPS_PERIOD = 1E+3/MAX_UPS;
     private boolean isRunning = false;
     private SurfaceHolder surfaceHolder;
     private Game game;
+    private double averageUPS;
+    private double averageFPS;
 
     public GameLoop(Game game, SurfaceHolder surfaceHolder) {
         this.surfaceHolder = surfaceHolder;
@@ -14,10 +18,10 @@ public class GameLoop extends Thread{
     }
 
     public double getAvarageUPS() {
-        return 0;
+        return averageUPS;
     }
     public double getAvarageFPS() {
-        return 0;
+        return averageFPS;
     }
 
     public void startLoop() {
@@ -28,7 +32,15 @@ public class GameLoop extends Thread{
     @Override
     public void run() {
         super.run();
+        int updateCount = 0;
+        int frameCount = 0;
+
+        long startTime;
+        long elapsedTime;
+        long sleepTime;
+
         Canvas canvas;
+        startTime = System.currentTimeMillis();
         while (isRunning) {
             try {
                 canvas = surfaceHolder.lockCanvas();
@@ -37,6 +49,28 @@ public class GameLoop extends Thread{
                 surfaceHolder.unlockCanvasAndPost(canvas);
             } catch (IllegalArgumentException e) {
                 e.printStackTrace();
+            }
+
+            updateCount++;
+            frameCount++;
+
+            elapsedTime = System.currentTimeMillis() - startTime;
+            sleepTime = (long)(updateCount * UPS_PERIOD - elapsedTime);
+            if (sleepTime > 0) {
+                try {
+                    sleep(sleepTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            elapsedTime = System.currentTimeMillis() - startTime;
+            if (elapsedTime >= 1000) {
+                averageUPS = updateCount / (1E-3 * elapsedTime);
+                averageFPS = frameCount / (1E-3 * elapsedTime);
+                updateCount = 0;
+                frameCount = 0;
+                startTime = System.currentTimeMillis();
             }
         }
     }
